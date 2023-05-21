7e925840d48d116d10cbf57cac3b08651957ad8a
Surfer-liner <130780662+Surfer-liner@users.noreply.github.com>
2023-05-21 17:53:20 +0600

    Update forbes_rss_content_parser.py

--- forbes_rss_content_parser.py
+++ forbes_rss_content_parser.py
@@ -6,12 +6,11 @@ import os
 # ==============================================================================
 rss_url = 'https://www.forbes.ru/newrss.xml'
 processed_links_file = 'processed_links.txt'
-categories = ['Инвестиции', 'Миллиардеры', 'Бизнес', 'Финансы', 'Технологии',
-              'Карьера и свой бизнес']
+categories = ['ADD_YOUR_CATEGORY']
 # ==============================================================================
 
 def get_link_hash(link):
-    """Возвращает хеш ссылки"""
+    """"""
     link_hash = hashlib.sha1(link.encode()).hexdigest()
     return link_hash
 
@@ -21,7 +20,6 @@ def parse_forbes_rss(rss_url, categories):
     rss_content = response.text
     soup = BeautifulSoup(rss_content, 'xml')
 
-    # Проверка наличия файла с обработанными ссылками
     processed_links = []
     if os.path.exists(processed_links_file):
         with open(processed_links_file, 'r') as f:
@@ -29,37 +27,30 @@ def parse_forbes_rss(rss_url, categories):
 
     parsed_items = []
 
-    # Парсинг каждого элемента (статьи) в RSS-ленте
     items = soup.find_all('item')
     for item in items:
-        # Извлечение заголовка, описания, ссылки на статью и категории
         title = item.find('title').text.strip()
         description = item.find('description').text.strip()
         article_url = item.find('link').text.strip()
         categories_tags = item.find_all('category')
 
-        # Проверка наличия ссылки на статью в списке обработанных ссылок
         if article_url in processed_links:
-            continue  # Пропускаем уже обработанную ссылку
+            continue  
 
-        # Извлечение категорий и проверка на их наличие в списке выбранных категорий
         article_categories = [category.text.strip() for category in categories_tags]
         if not any(category in article_categories for category in categories):
-            continue  # Пропускаем статью, если нет выбранных категорий
+            continue 
 
-        # Получение ссылки на изображение из элемента enclosure (если доступно)
         image_url = None
         enclosure = item.find('enclosure')
         if enclosure and 'url' in enclosure.attrs:
             image_url = enclosure['url']
 
-        # Сохранение изображения (если доступно)
         if image_url:
             image_filename = download_image(image_url)
         else:
             image_filename = None
 
-        # Создание словаря с данными статьи
         article_data = {
             'title': title,
             'description': description,
@@ -69,10 +60,8 @@ def parse_forbes_rss(rss_url, categories):
         }
         parsed_items.append(article_data)
 
-        # Добавление ссылки на статью в список обработанных ссылок
         processed_links.append(article_url)
 
-    # Сохранение списка обработанных ссылок в файл
     with open(processed_links_file, 'w') as f:
         f.write('\n'.join(processed_links))
 
@@ -80,16 +69,14 @@ def parse_forbes_rss(rss_url, categories):
 
 
 def download_image(image_url):
-    """Скачивает изображение по указанной ссылке и возвращает имя сохраненного файла"""
+    """"""
     response = requests.get(image_url)
     image_content = response.content
     image_extension = os.path.splitext(image_url)[1]
     image_filename = f'{get_link_hash(image_url)}{image_extension}'
 
-    # Создание папки для хранения изображений (если не существует)
     os.makedirs('images', exist_ok=True)
 
-    # Сохранение изображения
     image_path = os.path.join('images', image_filename)
     with open(image_path, 'wb') as f:
         f.write(image_content)
